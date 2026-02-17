bb76f1f56c145092a20b288df85d0962e5ea70ec
Anadasu Shethu Babu <shethu.babu@bounteous.com>
2026-02-17 15:47:01 +0530

    image url

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import { Card, CardHeader, CardTitle, CardContent } from "./ui/card"
+import {fetchPokemonDetails} from "../api/pokemonApi"
+import { useEffect, useState } from "react";
 
 interface Props {
   name: string;
 }
 
 const PokemonCard = ({ name }: Props) => {
+
+  const [imgUrl, setImgUrl] = useState<string>("");
+    useEffect(() => {
+    const loadPokemon = async () => {
+      const data = await fetchPokemonDetails(name);
+      setImgUrl(data.sprites.other["official-artwork"].front_default);
+    };
+
+    loadPokemon();
+  }, [name]);
   return (
     <Link to={`/pokemon/${name}`}>
       <Card className="hover:scale-105 transition">
@@ -16,7 +28,7 @@ const PokemonCard = ({ name }: Props) => {
         </CardHeader>
         <CardContent>
           <img
-            src={`https://img.pokemondb.net/artwork/${name}.jpg`}
+            src={imgUrl}
             alt={name}
             className="w-full h-40 object-contain"
           />
@@ -27,3 +39,9 @@ const PokemonCard = ({ name }: Props) => {
 };
 
 export default PokemonCard;
+
+
+function setImgUrl(front_default: any) {
+  throw new Error("Function not implemented.");
+}
+
